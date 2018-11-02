8edbda5a53ab97f43ec719cc06cc02fa34864f81
huangzezhou <764804602@qq.com>
2018-11-02 21:19:17 +0800

    token有效期设置为第二天凌晨2点

--- src/main/java/com/justplay1994/github/unifiedauth/service/Impl/JwtServiceImpl.java
+++ src/main/java/com/justplay1994/github/unifiedauth/service/Impl/JwtServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.UUID;
 
@@ -28,13 +29,9 @@ public class JwtServiceImpl {
     private static final String serverSecret = UUID.randomUUID().toString();
 
     public boolean validateToken(String token) throws UnsupportedEncodingException {
-        try {
-            SecretKeySpec keySpec = new SecretKeySpec(serverSecret.getBytes("UTF-8"), "HmacSHA256");
-            Jwts.parser().setSigningKey(keySpec).parseClaimsJws(token);
-            return true;
-        } catch (JwtException e) {
-            return false;   //正常逻辑，token校验失败，该方法直接处理。
-        }
+        SecretKeySpec keySpec = new SecretKeySpec(serverSecret.getBytes("UTF-8"), "HmacSHA256");
+        Jwts.parser().setSigningKey(keySpec).parseClaimsJws(token);
+        return true;
     }
 
     /**
@@ -56,11 +53,14 @@ public class JwtServiceImpl {
      */
     public String token(String account) throws UnsupportedEncodingException, InterruptedException {
         SecretKeySpec keySpec = new SecretKeySpec(serverSecret.getBytes("UTF-8"), "HmacSHA256");
+        //有效期设置为第二天的凌晨2点
+        Calendar c = Calendar.getInstance();
+        c.set(c.YEAR, c.MONTH+1, c.DAY_OF_MONTH+1, 2, 0);
         return Jwts.builder()
                 .setIssuer("CETC")  //签发人
                 .setSubject(account)    //客体-被校验的用户
                 .setAudience("CETC ") //接收token方
-                .setExpiration(new Date(new Date().getTime() + 15 * 60 * 1000)) //有效期至该时间
+                .setExpiration(c.getTime()) //有效期至该时间
                 .setIssuedAt(new Date())
                 .setId(String.valueOf(UUID.randomUUID()))
                 .signWith(keySpec).compact();
